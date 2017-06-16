3d8f0a89c1ff6a27fb7afb1f57f1539d9a015058
NightFarmer <NightFarmer@163.com>
2017-06-16 13:13:52 +0800

    新增示例

--- sample/src/main/java/com/nightfarmer/progressview/sample/MainActivity.kt
+++ sample/src/main/java/com/nightfarmer/progressview/sample/MainActivity.kt
@@ -31,6 +31,43 @@ class MainActivity : AppCompatActivity() {
             progressView?.state = ProgressView.ProgressState.Loading
             startLoadingThread()
         }
+
+        initOtherView()
+    }
+
+    fun initOtherView() {
+        progressView2.radius = 0f
+        progressView2.borderWidth = 1f
+        progressView2.textSize = 40f
+        progressView2.progress = 0.7f
+        progressView2.state = ProgressView.ProgressState.Paused
+        progressView2.color = ContextCompat.getColor(this,R.color.downloadButton2)
+
+        progressView3.radius = 100f
+        progressView3.borderWidth = 5f
+        progressView3.textSize = 40f
+        progressView3.progress = 0.49f
+        progressView3.state = ProgressView.ProgressState.Loading
+        progressView3.color = ContextCompat.getColor(this,R.color.downloadButton3)
+
+        progressView4.radius = 30f
+        progressView4.borderWidth = 2f
+        progressView4.color = ContextCompat.getColor(this,R.color.downloadButton4)
+        progressView4.idleText = "冒泡"
+        progressView4.pausedText = "潜水"
+
+        progressView5.radius = 5f
+        progressView5.borderWidth = 1f
+        progressView5.textSize = 20f
+        progressView5.progress = 1f
+        progressView5.state = ProgressView.ProgressState.Finished
+        progressView5.color = ContextCompat.getColor(this,R.color.downloadButton5)
+
+        progressView6.radius = 5f
+        progressView6.borderWidth = 1f
+        progressView6.textSize = 20f
+        progressView6.color = ContextCompat.getColor(this,R.color.downloadButton6)
+
     }
 
     var loadingThread: LoadingThread? = null
